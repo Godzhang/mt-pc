087afb8be4b40192659c8c9d71b060f7931e50c4
godzhang <zhangqijilindaxue@126.com>
2020-04-05 08:27:24 +0800

    init config and modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
-}
+    "nuxt/no-cjs-in-config": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-closing-bracket-newline": "off",
+    "vue/html-indent": "off",
+    semi: "off",
+    "comma-dangle": "off",
+    quotes: "off",
+  },
+};
